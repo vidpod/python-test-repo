2e7744b0b9da6a27b4f529207597dcbab3ab7fc9
vidpod <vid.podrekar@student.um.si>
2026-04-05 13:43:56 +0200

    Adjust sleep duration between print statements
    Added time.sleep(3) to each print statement for pacing.

--- run.py
+++ run.py
@@ -3,10 +3,12 @@ import time
 try:
     while True:
         print("day is never finished", flush=True)
+        time.sleep(3)
         print("master got me working", flush=True)
+        time.sleep(3)
         print("someday master set me free", flush=True)
+        time.sleep(3)
         print("--------------------------", flush=True)
-        time.sleep(2)
 
 except KeyboardInterrupt:
     print("\nProgram ustavljen (CTRL+C)")
