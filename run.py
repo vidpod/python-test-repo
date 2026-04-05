3e9191c0be08745a55df61193b69f286b08ed1ff
vidpod <vid.podrekar@student.um.si>
2026-04-05 11:17:48 +0000

    cartman

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
-print("Python aplikacija deluje v Docker containerju!")
+import time
+
+try:
+    while True:
+        print("day is never finished")
+        print("master got me working")
+        print("someday master set me free")
+        print("--------------------------")
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print("\nProgram ustavljen (CTRL+C)")
