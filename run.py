caa81428c3f049f4a35d534e9e114de8adef4c0b
vidpod <vid.podrekar@student.um.si>
2026-04-05 13:40:52 +0200

    Fix print statements in run.py

--- run.py
+++ run.py
@@ -2,10 +2,10 @@ import time
 
 try:
     while True:
-        print("day is never finished", flush=True))
-        print("master got me working", flush=True))
-        print("someday master set me free", flush=True))
-        print("--------------------------", flush=True))
+        print("day is never finished", flush=True)
+        print("master got me working", flush=True)
+        print("someday master set me free", flush=True)
+        print("--------------------------", flush=True)
         time.sleep(2)
 
 except KeyboardInterrupt:
